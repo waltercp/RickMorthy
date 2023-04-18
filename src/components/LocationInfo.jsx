900092303f55de584c11974bb38ba73f130fdf9c
waltercp <walter_cp_96@hotmail.com>
2023-04-17 19:03:49 -0600

    Segunda prueva

--- src/components/LocationInfo.jsx
+++ src/components/LocationInfo.jsx
@@ -10,9 +10,9 @@ const LocationInfo = ({ location }) => {
     <div className='location' >
       <h2 className='location_name' >{location?.name}</h2>
       <ul className='location_list' >
-        <li className='location_intem' > <span>Type</span>{location?.type} </li>
-        <li className='location_intem' > <span>Dimension:</span>{location?.dimension}</li>
-        <li className='location_intem' > <span>Population:</span>{location?.residents.length}</li>
+        <li className='location_intem' > <span>Type</span> <span>{location?.type}</span> </li>
+        <li className='location_intem' > <span>Dimension:</span><span>{location?.dimension}</span></li>
+        <li className='location_intem' > <span>Population:</span><span>{location?.residents.length}</span></li>
       </ul>
 
 
