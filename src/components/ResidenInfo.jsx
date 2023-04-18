7ad7fa11706bdff0264eb12b587aa93d9aa33541
waltercp <walter_cp_96@hotmail.com>
2023-04-17 21:18:45 -0600

    Cuarta Prueba

--- src/components/ResidenInfo.jsx
+++ src/components/ResidenInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import useFetch from '../hooks/useFetch'
 import './syles/residentInfo.css'
+import Loading from './Loading'
 
 
 
@@ -20,24 +21,30 @@ const ResidenInfo = ({ url }) => {
 
     return (
         <article className='resident'>
-            <header className='resident_header'>
-                <img className='resident_img' src={resident?.image} alt="" />
-                <div className='resident_status'>
-                    <div className={`resident_status-circle ${resident?.status}`}></div>
-                    <span className='resident_status-label'>{resident?.status}</span>
-                </div>
-            </header>
-            <section className='resident_body'>
-                <h3 className='resident_name'>{resident?.name}</h3>
-                <hr className='resident_hr' />
-                <ul className='resident_list'>
-                    <li className='resident_item'> <span>species </span>  <span>{resident?.species}</span> </li>
-                    <li className='resident_item'> <span>Origin</span>  <span>{resident?.origin.name}</span> </li>
-                    <li className='resident_item'> <span>Eppisodes where appear</span>  <span>{resident?.episode.length}</span> </li>
-                </ul>
-            </section>
+            {
+                resident
+                    ? <>
+                        <header className='resident_header'>
+                            <img className='resident_img' src={resident?.image} alt="" />
+                            <div className='resident_status'>
+                                <div className={`resident_status-circle ${resident?.status}`}></div>
+                                <span className='resident_status-label'>{resident?.status}</span>
+                            </div>
+                        </header>
+                        <section className='resident_body'>
+                            <h3 className='resident_name'>{resident?.name}</h3>
+                            <hr className='resident_hr' />
+                            <ul className='resident_list'>
+                                <li className='resident_item'> <span>species </span>  <span>{resident?.species}</span> </li>
+                                <li className='resident_item'> <span>Origin</span>  <span>{resident?.origin.name}</span> </li>
+                                <li className='resident_item'> <span>Eppisodes where appear</span>  <span>{resident?.episode.length}</span> </li>
+                            </ul>
+                        </section>
+                    </>
+                    :Loading
+            }
 
-          
+
 
         </article>
 
